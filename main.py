4c23a51aaf98f1fcbd1050e98ca36c8f32c077ba
mingor <mmmmingor@gmail.com>
2021-01-29 17:06:16 +0800

    implement EB-DBA template method

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 import time
+import random
 
 
 class my_timer():
@@ -128,12 +129,32 @@ def plot_signatures(read_fun, user_no: int, sig_num: int, inverse_axis=False):
     plt.show()
 
 
+def plot_signatures_features(read_fun, user_no, sig_num):
+    sig_array = [read_fun(user_no, i + 1) for i in range(sig_num)]
+
+    columns = sig_array[0].columns
+    columns_len = len(columns)
+
+    fig, ax_arr = plt.subplots(columns_len)
+
+    for idx, column in enumerate(columns):
+        ax = ax_arr[idx]
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_ylabel(column)
+        for sig in range(sig_num):
+            data = sig_array[sig][column]
+            ax.plot(data)
+
+    plt.show()
+
+
 def pcolormesh_DTW(read_fun, user_no: int, sig_num: int, verbose=False):
     """
     example:
             pcolormesh_DTW(read_MMSIG, 1, 40)
     """
-    sig_array = [read_fun(user_no, i + 1) for i in range(sig_num)]
+    sig_array = [read_fun(user_no, i + 1).to_numpy() for i in range(sig_num)]
 
     dist_mesh = np.zeros((sig_num, sig_num))
 
@@ -158,12 +179,12 @@ def pcolormesh_DTW(read_fun, user_no: int, sig_num: int, verbose=False):
     plt.show()
 
 
-def DTW(lhs: pd.DataFrame, rhs: pd.DataFrame):
+def DTW(lhs: pd.DataFrame, rhs: pd.DataFrame, with_path=False):
     """
     dependent dynamic timing warping
         lhs and rhs should have same number of features.
     """
-    lhs, rhs = lhs.to_numpy().T, rhs.to_numpy().T
+    lhs, rhs = lhs.T, rhs.T
     features_num, N = lhs.shape
     _, M = rhs.shape
 
@@ -193,11 +214,31 @@ def DTW(lhs: pd.DataFrame, rhs: pd.DataFrame):
             D[i, j] += min(D[i - 1, j], D[i - 1, j - 1], D[i, j - 1])
 
     avg_L = np.sqrt(M * N)
+    result = D[N - 1, M - 1] / avg_L
 
-    return D[N - 1, M - 1] / avg_L
+    if with_path == False:
+        return result
+
+    # get the DTW path
+    path = []
+    i, j = N - 1, M - 1
+    path.append((i, j))
+    while i > 0 or j > 0:
+        idx = np.argmin([D[i - 1, j], D[i, j - 1], D[i - 1, j - 1]])
+        if idx == 0:
+            i = i - 1
+        elif idx == 1:
+            j = j - 1
+        else:
+            i, j = i - 1, j - 1
+        path.append((i, j))
+
+    path = path[:: -1]
+
+    return result, path
 
 
-def user_independent_ROC(users_num: int, training: int, genuine: int, forged: int, DTW_matrix) -> float:
+def user_independent_ROC(users_num: int, training: int, genuine: int, forged: int, DTW_matrix, title="no title") -> float:
     threshold_array = np.arange(0, 5, 0.01, dtype="float")
     FA = np.zeros(len(threshold_array), dtype="int")
     FR = np.zeros(len(threshold_array), dtype="int")
@@ -233,6 +274,7 @@ def user_independent_ROC(users_num: int, training: int, genuine: int, forged: in
     plt.xlabel("False Reject Rate")
     plt.ylabel("False Acept Rate")
     plt.legend()
+    plt.title(title)
     # plt.show()
 
     print(f"user-independent EER: {EER}, threshold: {threshold_array[idx]}")
@@ -290,7 +332,8 @@ def single_tpl_min_based_classify(read_fun, users_num: int, training: int, genui
     with my_timer("loading signature... "):
         users_data = []
         for u in range(users_num):
-            sig_arr = [read_fun(u + 1, sig + 1) for sig in range(sig_num)]
+            sig_arr = [read_fun(u + 1, sig + 1).to_numpy()
+                       for sig in range(sig_num)]
             users_data.append(sig_arr)
 
     # finding the signature has minimum DTW with others
@@ -315,12 +358,12 @@ def single_tpl_min_based_classify(read_fun, users_num: int, training: int, genui
             for sig in range(training, sig_num):
                 DTW_all[u, sig] = DTW(users_data[u][sig], training_data[u])
 
-    # # calculate FAR, FRR, EER
+    # calculate FAR, FRR, EER
     with my_timer("calculating EER... "):
         ERR = user_dependent_ROC(users_num, training,
                                  genuine, forged, DTW_all)
         ERR = user_independent_ROC(users_num, training,
-                                   genuine, forged, DTW_all)
+                                   genuine, forged, DTW_all, "min-based single template")
 
 
 def mul_tpl_mean_based_classify(read_fun, users_num: int, training: int, genuine: int, forged: int):
@@ -333,7 +376,8 @@ def mul_tpl_mean_based_classify(read_fun, users_num: int, training: int, genuine
     with my_timer("loading signature... "):
         users_data = []
         for u in range(users_num):
-            sig_arr = [read_fun(u + 1, sig + 1) for sig in range(sig_num)]
+            sig_arr = [read_fun(u + 1, sig + 1).to_numpy()
+                       for sig in range(sig_num)]
             users_data.append(sig_arr)
 
     # calculate DTW
@@ -355,12 +399,116 @@ def mul_tpl_mean_based_classify(read_fun, users_num: int, training: int, genuine
         ERR = user_dependent_ROC(users_num, training,
                                  genuine, forged, DTW_mean)
         ERR = user_independent_ROC(users_num, training,
-                                   genuine, forged, DTW_mean)
+                                   genuine, forged, DTW_mean, "mean-based multiple template")
+
+
+def EB_DBA_based_classify(read_fun, users_num: int, training: int, genuine: int, forged: int, times: int):
+    if training <= 0 or genuine < training or forged < 0:
+        raise Exception("args has error")
+
+    sig_num = genuine + forged
+
+    # loading signature
+    with my_timer("loading signature... "):
+        users_data = []
+        for u in range(users_num):
+            sig_arr = [read_fun(u + 1, sig + 1).to_numpy()
+                       for sig in range(sig_num)]
+            users_data.append(sig_arr)
+
+    # linerear interpolation
+    with my_timer("linerear interpolation... "):
+        features_num = users_data[0][0].shape[-1]
+        original_L = []
+        resample_L = []
+        for u in range(users_num):
+            L_sum, L_avg = 0, 0
+            origin_L, resam_L = [], []
+            for sig in range(training):
+                L = users_data[u][sig].shape[0]
+                L_sum += L
+                origin_L.append(np.arange(0, L))
+
+            avg_L = int(L_sum / training)
+            for sig in range(training):
+                L = origin_L[sig][-1]
+                resam_L.append(np.arange(0, L, L / avg_L)[0: avg_L])
+
+            original_L.append(origin_L)
+            resample_L.append(resam_L)
+
+        new_users_data = []
+        for u in range(users_num):
+            new_data = []
+            for sig in range(training):
+                new_sig = []
+                for feat in range(features_num):
+                    interp = np.interp(
+                        resample_L[u][sig], original_L[u][sig], users_data[u][sig][:, feat])
+                    new_sig.append(interp)
+
+                new_data.append(np.array(new_sig).T)
+
+            new_users_data.append(new_data)
+
+    # calculate the EB-DBA
+    with my_timer("calculating EB-DBA..."):
+        # calculate the Euclidean Barycenter between training data.
+        EB_data = []
+        for u in range(users_num):
+            sum_sig = np.zeros(new_users_data[u][0].shape)
+            for sig in range(training):
+                sum_sig += new_users_data[u][sig]
+
+            EB_data.append(sum_sig / training)
+
+        # calculate the EB-DBA signature by EB
+        EB_DBA_data = EB_data.copy()
+        for u in range(users_num):
+            for t in range(times):
+                L = EB_data[u].shape[0]
+                assoc_set = [[] for l in range(L)]
+                for sig in range(training):
+                    W, path = DTW(users_data[u][sig],
+                                  EB_DBA_data[u], with_path=True)
+
+                    for n, l in path:
+                        # n, l = path[p]
+                        assoc_set[l].append(users_data[u][sig][n])
+
+                DBA = np.zeros((L, features_num))
+                for l in range(L):
+                    DBA[l] = (np.mean(assoc_set[l], axis=0))
+                EB_DBA_data[u] = DBA
+
+    for u in range(users_num):
+        for sig in range(training):
+            plt.plot(users_data[u][sig][:, 0], users_data[u][sig][:, 1], "--")
+        plt.plot(EB_DBA_data[u][:, 0], EB_DBA_data[u][:, 1], "black", linewidth=3)
+        plt.show()
+
+    # calculate DTW with the EB-DBA signature
+    with my_timer("calculating DTW..."):
+        DTW_all = np.zeros((users_num, sig_num))
+        for u in range(users_num):
+            for sig in range(training, sig_num):
+                DTW_all[u, sig] = DTW(users_data[u][sig], EB_DBA_data[u])
+
+    # calculate FAR, FRR, EER
+    with my_timer("calculating EER... "):
+        ERR = user_dependent_ROC(users_num, training,
+                                 genuine, forged, DTW_all)
+        ERR = user_independent_ROC(users_num, training,
+                                   genuine, forged, DTW_all, "EB-DBA based single template")
 
 
 if __name__ == "__main__":
+    user_no = random.randint(1, 41)
+    sig_num = 40
 
-    # plot_signatures(read_MMSIG, 26, 40)
+    # plot_signatures(read_SVC2004, user_no, sig_num)
+
+    # plot_signatures_features(read_SVC2004, user_no, sig_num)
 
     # pcolormesh_DTW(read_MMSIG, 26, 40, True)
 
@@ -368,7 +516,9 @@ if __name__ == "__main__":
     training = 5
     genuine = 20
     forged = 20
-    single_tpl_min_based_classify(read_SVC2004, users_num,
-                                  training, genuine, forged)
+    times = 5
+    EB_DBA_based_classify(read_SVC2004, users_num,
+                          training, genuine, forged, times)
+
 
     pass
